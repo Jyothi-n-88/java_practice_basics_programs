abd128286e146332d15f1b6266efddf597623d90
Jyothi.N <jyothin882006@gmail.com>
2026-05-09 16:45:20 +0530

    Simple Type_promotion java prg

--- type_promotion.java
+++ type_promotion.java
@@ -6,5 +6,9 @@ public class type_promotion
         char b='b';
         System.out.println((int)b);
         System.out.println(a+b);//automatic type promotion from short , char to int
+        byte c=5;
+        // c=c*2; <-wrong automatic type conversion takes place to int 
+        c=(byte)(c*2);
+        System.out.println(c);
     }
 }
