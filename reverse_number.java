e8d18c6ebb91c890284fde5a598ade051d1ea639
Jyothi.N <jyothin882006@gmail.com>
2026-05-19 20:21:17 +0530

    Reverse the given number

--- reverse_number.java
+++ reverse_number.java
@@ -2,13 +2,14 @@ public class javabasics{
     public static void main(String args[])
     {
         int n=10899;
+        int rev=0;
         while(n>0)
         {
             int lastdigit=n%10;
-            System.out.print(lastdigit);
+            rev=(rev*10)+lastdigit;
             n=n/10;
         }
-        System.out.println();
+        System.out.println(rev);
         
     }
 }
