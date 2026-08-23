5233084e7ce9f90797af4a8174b1f5363f5ef57f
Jyothi.N <jyothin882006@gmail.com>
2026-08-23 22:58:16 +0530

    Max Subarray sum(kadane's algo)

---
+++ maxsubarraysum3.java
@@ -0,0 +1,24 @@
+
+public class maxsubarraysum3 {
+    public static void max_sub_arrays(int array[]) { 
+        int currentsum = 0;
+        int maxSum=0;
+        for(int i=0;i<array.length;i++)
+        {
+            currentsum=currentsum+array[i];
+            if(currentsum<0)
+            {
+                currentsum=0;
+            }
+            maxSum=Math.max(currentsum,maxSum);
+
+        }
+        
+        System.out.println("Max-Sum :"+maxSum);
+    }
+    public static void main(String[] args) {
+        int array[] = {-2,-3,4,-1,-2,1,5,-3};
+        max_sub_arrays(array);
+    }
+}
+ 
