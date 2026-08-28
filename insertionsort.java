e8a4b9a729d552f5eb487e1bfa964c972dcaf835
Jyothi.N <jyothin882006@gmail.com>
2026-08-28 16:49:34 +0530

    Insertion Sort

---
+++ insertionsort.java
@@ -0,0 +1,22 @@
+public class InsertionSort {
+
+    public static void main(String[] args) {
+
+        int[] arr = {5, 3, 8, 4, 2};
+        int i, j, key;
+        for (i = 1; i < arr.length; i++) {
+            key = arr[i];
+            j = i - 1;
+            while (j >= 0 && arr[j] > key) {
+
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+
+        for (i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
