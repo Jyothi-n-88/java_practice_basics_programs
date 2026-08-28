617031a30f1ece85c0093cdbe2aa282e8210da3c
Jyothi.N <jyothin882006@gmail.com>
2026-08-28 11:49:07 +0530

    Factorial-Recursion-method

---
+++ factorialrecursion.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class factorialrecursion
+{
+    static int fact(int n)
+    {
+         if(n<=1)
+         {
+            return 1;
+         }
+         return n*fact(n-1);
+    }
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter number");
+        int n=sc.nextInt();
+        System.out.println("The factorial of a number is: "+fact(n));
+    }
+}
