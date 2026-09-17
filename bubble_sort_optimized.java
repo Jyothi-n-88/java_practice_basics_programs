eac95c86135ad7b25a639360fe0a6a73c2d05c61
Jyothi.N <jyothin882006@gmail.com>
2026-09-17 23:51:26 +0530

    Bubble sort with TC-O(n)

---
+++ bubble_sort_optimized.java
@@ -0,0 +1,39 @@
+public class bubble_sort_optimized{
+
+    public static void bubbleSort(int arr[]) {
+
+        for (int i = 0; i < arr.length - 1; i++) {
+
+            boolean swapped = false;
+
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+
+                if (arr[j] > arr[j + 1]) {
+
+                    // Swap
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+
+                    swapped = true;
+                }
+            }
+
+            // No swapping means array is already sorted
+            if (swapped == false) {
+                break;
+            }
+        }
+    }
+
+    public static void main(String args[]) {
+
+        int arr[] = {1, 2, 3, 4, 5};
+
+        bubbleSort(arr);
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
