0653d47e33aabd5f3df06795ad3614ebd92a80da
Jyothi.N <jyothin882006@gmail.com>
2026-09-25 00:01:26 +0530

    Finding Largest String

---
+++ largeststring.java
@@ -0,0 +1,18 @@
+//Ques: For a given set of string print largest string
+
+class largeststring 
+{
+    public static void main(String args[])
+    {
+        String str[]={"apple","mango","banana"};
+        String largest=str[0];
+        for(int i=1;i<str.length;i++)
+        {
+            if(largest.compareTo(str[i])<0)
+            {
+                largest=str[i];
+            }
+        }
+        System.out.println("The Largest String is: "+largest);
+    }
+}
